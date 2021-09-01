8dcbd2d2768c9413eab938de3c5d63744fa3b8e4
Konstantin <zubovdev@gmail.com>
2021-09-01 19:23:28 +0500

    #398 - Small code changes.

--- engine/cursor/cursor.go
+++ engine/cursor/cursor.go
@@ -1,32 +1,37 @@
 package cursor
 
 import (
+	"errors"
+	"fmt"
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/skycoin/cx-game/engine/spriteloader"
 	"image"
-	"log"
 	"strings"
 )
 
-var cursors = make(map[string]*glfw.Cursor)
+var (
+	cursors = make(map[string]*glfw.Cursor)
+
+	ErrUnknownCursor = errors.New("unknown cursor")
+)
 
 // Get returns the cursor by name.
-// If the cursor does not exist, a fatal error will occur.
-func Get(name string) *glfw.Cursor {
+// If the cursor does not exist, an error will be returned.
+func Get(name string) (*glfw.Cursor, error) {
 	// Check whether cursor exist.
 	cursor, ok := cursors[name]
 	if !ok {
-		log.Fatalf("Unknown cursor %s", name)
+		return nil, ErrUnknownCursor
 	}
-	return cursor
+	return cursor, nil
 }
 
 // Load loads the cursors defined in the config.
-func Load(configPath, spritePath string) {
+func Load(configPath, spritePath string) error {
 	// Reading the sprite sheet config.
 	sheetConfig, err := spriteloader.ReadSpriteSheetConfig(configPath)
 	if err != nil {
-		log.Fatalln("failed to load sprite sheet config for the cursors:", err)
+		return fmt.Errorf("failed to load sprite sheet config for the cursors: %v", err)
 	}
 
 	// Loading image.
@@ -43,4 +48,5 @@ func Load(configPath, spritePath string) {
 		// Registering the cursor.
 		cursors[cursorName] = glfw.CreateCursor(img, 0, 0)
 	}
+	return nil
 }
